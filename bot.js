47b28b2b313e57dad8663e17be361bb9a92ab0ef
LORD14GAMER <46014288+LORD14GAMER@users.noreply.github.com>
2019-02-01 20:32:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const preifx = "&"
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -245,6 +246,50 @@ client.on('message', message => {
 }
 
 });
+
+
+
+
+client.on('message', message => {
+  if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + "bc")) {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );  //OUAIL
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let copy = "BC-Bot";
+let request = `Requested By ${message.author.username}`;  //OUAIL
+if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))  //OUAIL
+.then(() =>msg.react('✅'))  //OUAIL
+
+
+
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });  //OUAIL
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });  //OUAIL
+reaction1.on("collect", r => {
+message.channel.send(`☑ |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو `).then(m => m.delete(5000));  //OUAIL
+message.guild.members.forEach(m => {
+var bc = new  
+Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle('البرودكاست') .addField('السيرفر', message.guild.name) .addField('المرسل', message.author.username)  //OUAIL
+.addField('الرساله', args)  //OUAIL
+.setThumbnail(message.author.avatarURL)  //OUAIL
+.setFooter(copy, client.user.avatarURL); //OUAIL
+m.send({ embed: bc })
+msg.delete();  //OUAIL
+})
+})
+reaction2.on("collect", r => {  //OUAIL
+message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));   //OUAIL
+msg.delete();  //OUAIL
+})  //OUAIL
+}) //OUAIL
+}  //OUAIL
+}) //OUAIL
 
 
 
